eaab2203548c86214256e909e160eeec699a06fa
Alam <alamkhalid0803@gmail.com>
2020-05-31 05:00:18 +0500

    http reqs with axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,51 @@
-import React, { Component } from 'react';
-
-import './FullPost.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  state = { post: null };
 
+  async componentDidUpdate(prevProps, prevState) {
+    if (this.props.id) {
+      if (
+        !this.state.post ||
+        (this.state.post && this.props.id !== this.state.post.id)
+      ) {
+        const { data: post } = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts/" + this.props.id
         );
-        return post;
+        this.setState({ post });
+      }
     }
+  }
+
+  deletePostHandler = async () => {
+    await axios.delete(
+      "https://jsonplaceholder.typicode.com/posts/" + this.props.id
+    );
+  };
+
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = <p style={{ textAlign: "center" }}>Loading</p>;
+    }
+    if (this.state.post) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.post.title}</h1>
+          <p>{this.state.post.body}</p>
+          <div className="Edit">
+            <button onClick={this.deletePostHandler} className="Delete">
+              Delete
+            </button>
+          </div>
+        </div>
+      );
+    }
+
+    return post;
+  }
 }
 
 export default FullPost;
